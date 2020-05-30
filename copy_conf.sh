37d73b2f7bc422190a5d7bef3667cd5783ae668e
Yevgeny Kozin <evgeniy.kozin@gmail.com>
2020-05-30 22:31:33 +0300

    Делаем соединения внутри контейнера без пароля

--- copy_conf.sh
+++ copy_conf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp -f /var/postgresql.conf /var/lib/postgresql/data/
-echo 'local\tall\tall\tmd5\nhost\tall\tall\t0.0.0.0/0\tmd5' > /var/lib/postgresql/data/pg_hba.conf
+echo 'local\tall\tall\ttrust\nhost\tall\tall\t0.0.0.0/0\tmd5' > /var/lib/postgresql/data/pg_hba.conf
